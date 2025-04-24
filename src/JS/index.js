25cf789c034d2a2a655b959efefb28bea957aed9
Sofiyka751 <140595863+Sofiyka751@users.noreply.github.com>
2025-04-24 17:09:17 +0300

    second commit

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,24 +1,44 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const newsList = document.getElementById("news-list");
+const loadMoreButton = document.getElementById("load");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const url = "https://rickandmortyapi.com/api";
+let page = 2;
 
-setupCounter(document.querySelector('#counter'))
+const fetchMovieCharacters = async (page = 1) => {
+  try {
+    const newsData = await fetch(`${url}/character?page=${page}`);
+    const { results, info } = await newsData.json();
+    makeHtml(results, info.pages);
+  } catch (error) {}
+};
+
+function makePagination(countOfPages) {
+  const numbers = [];
+  for (let i = 1; i <= countOfPages; i++) {
+    numbers.push(i);
+  }
+  renderPagination(numbers);
+}
+
+fetchMovieCharacters(page);
+
+function makeHtml(characters) {
+  const markup = characters
+    .map((character) => {
+      return `<li class="news-item">
+    <img src="${character.image}" />
+    <h1>${character.name}</h1>
+    <p>${character.species}</p>
+    <p>${character.status}</p>
+    </li>`;
+    })
+    .join();
+
+  //   newsList.innerHTML = markup;
+  newsList.insertAdjacentHTML("beforeend", markup);
+}
+
+loadMoreButton.addEventListener("click", async () => {
+  page++;
+  fetchMovieCharacters(page);
+});
